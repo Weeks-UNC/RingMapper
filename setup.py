8815982173bb8669819a74f1156bf547e359ac03
Patrick Irving <psirving@gmail.com>
2025-02-15 03:57:53 +0000

    chore: QoL changes to setup.py, add .gitattributes

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 #########################################################
 # Code to compile readMutStrings.pyx
 # Anthony Mustoe
@@ -7,17 +6,18 @@
 # This file is licensed under the terms of the MIT license
 #
 #########################################################
-
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-ext = Extension('readMutStrings',
-                sources=['readMutStrings.pyx'],
-                include_dirs=[numpy.get_include()])
+ext = Extension(
+    "readMutStrings", sources=["readMutStrings.pyx"], include_dirs=[numpy.get_include()]
+)
+ext = cythonize(ext, compiler_directives={"language_level": "2"})
 
-setup(name='readMutStrings',
-      ext_modules=cythonize(ext, compiler_directives={"language_level": "2"})
-      )
+setup(
+    name="RingMapper",
+    ext_modules=ext,
+    scripts=["ringmapper.py", "pairmapper.py"],
+)
