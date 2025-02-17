101b0ad05540af7028e9cb0e508fbe07d0ab879b
Patrick Irving <psirving@gmail.com>
2025-02-17 05:06:35 +0000

    chore: setup all scripts/submodules, expose pxd via pip install

--- setup.py
+++ setup.py
@@ -6,18 +6,33 @@
 # This file is licensed under the terms of the MIT license
 #
 #########################################################
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
 ext = Extension(
-    "readMutStrings", sources=["readMutStrings.pyx"], include_dirs=[numpy.get_include()]
+    name="ringmapper.readMutStrings",
+    sources=["ringmapper/readMutStrings.pyx"],
+    include_dirs=[numpy.get_include()],
 )
 ext = cythonize(ext, compiler_directives={"language_level": "2"})
 
 setup(
     name="RingMapper",
+    packages=["ringmapper"],
+    package_dir={"ringmapper": "./"},
+    package_data={"ringmapper": ["*.pxd"]},
+    # include_package_data=True,
+    py_modules=[
+        "ringmapper.ringmapper",
+        "ringmapper.pairmapper",
+        "ringmapper.ShapeMapper_MMS0_Mut_Filter",
+        "ringmapper.readMutStrings",
+    ],
     ext_modules=ext,
-    scripts=["ringmapper.py", "pairmapper.py"],
+    scripts=[
+        "./ringmapper/ringmapper.py",
+        "./ringmapper/pairmapper.py",
+        "./ringmapper/ShapeMapper_MMS0_Mut_Filter.py",
+    ],
 )
