da099000f4a10e299cfdf2c676fb865dae1ef00f
Patrick Irving <psirving@gmail.com>
2025-02-18 05:24:48 +0000

    chore: fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # This file is licensed under the terms of the MIT license
 #
 #########################################################
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 
@@ -19,16 +19,10 @@ ext = cythonize(ext, compiler_directives={"language_level": "2"})
 
 setup(
     name="RingMapper",
-    packages=["ringmapper"],
+    packages=find_packages(include=["ringmapper", "ringmapper.*"]),
     package_dir={"ringmapper": "./"},
     package_data={"ringmapper": ["*.pxd"]},
-    # include_package_data=True,
-    py_modules=[
-        "ringmapper.ringmapper",
-        "ringmapper.pairmapper",
-        "ringmapper.ShapeMapper_MMS0_Mut_Filter",
-        "ringmapper.readMutStrings",
-    ],
+    include_package_data=True,
     ext_modules=ext,
     scripts=[
         "./ringmapper/ringmapper.py",
